0aee76024c76b503c7ff46f8f764030b71c9e166
myusername <jinalpatel11121999@gmail.com>
2026-02-27 10:18:04 -0500

    feat: update Firebase configuration to use environment variables for sensitive data

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,14 +9,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCbclH1nS5O7IbWH5oEJsVL-LBWGUeEUhY",
-  authDomain: "goodnightinn-fe916.firebaseapp.com",
-  projectId: "goodnightinn-fe916",
-  storageBucket: "goodnightinn-fe916.firebasestorage.app",
-  messagingSenderId: "960107487429",
-  appId: "1:960107487429:web:252e7b09937899b62fd0ac",
-  measurementId: "G-ELQQ14GLFY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
